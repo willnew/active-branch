ce3c893408ecf7f6ed6357aa380ce3bd29a5855b
Will Liu <willliu@ML00060013.local>
2014-06-26 22:31:55 +0800

    init branch address

--- auto-branch.sh
+++ auto-branch.sh
@@ -21,13 +21,21 @@ if  [ $flag != "Y" ] && [ $flag != "y" ] ; then
     exit 1
 fi
 
-declare -a projs_to_branch=("fnd-webserver-endurance" "endurance-management-service")
+declare -a projs_to_branch=("event-management-service" "endurance-management-service")
 
 working_copies=$(find . -d 1 -type d)
 for working_copy in $working_copies; do
-    proj=$(svn info $working_copy | grep URL | sed "s/URL: https:\/\///g" | cut -d/ -f 4)
+    proj_URL=$(svn info $working_copy | grep URL | sed "s/URL:\ //g" )
+    proj=$(echo $proj_URL | sed 's/https:\/\///g' | cut -d/ -f 4)
     if contains projs_to_branch[@] "$proj"; then
         echo "original: $working_copy after: $proj" 
+        # 1. branch
+        # add if...else... to handle FWE
+        current_version=$(cat "$working_copy/build.xml" | grep "name=\"ivy.deliver.revision\"" | cut -d '"' -f 4 | cut -d "-" -f 1 | cut -d "." -f 1-3)
+        new_version_number=$(echo $current_version | cut -d "." -f 2 | awk '{print $1 +1}')
+        new_version=$(echo $current_version | sed 's/\.[0-9]*\./\.'${new_version_number}'\./g')
+        echo $(echo $proj_URL | sed 's/trunk/branches\/'$new_version'/g')
+        # 2. version bump
     fi
 done
 
